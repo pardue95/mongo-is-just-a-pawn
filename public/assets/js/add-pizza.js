4b78803341193119f9d7da2e59ccd469142ecaa8
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-01-19 17:27:22 -0600

    working pizza-list and formated date

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -69,6 +69,23 @@ const handlePizzaSubmit = event => {
     .catch(err => {
       console.log(err);
     });
+    
+fetch('/api/pizzas', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(formData)
+})
+  .then(response => response.json())
+  .then(postResponse => {
+    alert('Pizza created successfully!');
+    console.log(postResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
