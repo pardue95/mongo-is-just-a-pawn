bd2602a9f5b18e3e2200cdbe041aec8189086153
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-01-19 10:08:13 -0600

    routes working in insomnia

--- models/Pizza.js
+++ models/Pizza.js
@@ -18,8 +18,6 @@ const PizzaSchema = new Schema({
   toppings: []
 });
 
-// create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
-// export the Pizza model
 module.exports = Pizza;
